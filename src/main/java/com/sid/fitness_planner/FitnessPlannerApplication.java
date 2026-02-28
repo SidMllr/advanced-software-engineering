c27db3e43c2e80175cdd05d6dedef88184fd2239
WeintraubenWilli <sid.salvador.mueller@gmail.com>
2026-02-28 20:45:32 +0100

    Commited project files

---
+++ src/main/java/com/sid/fitness_planner/FitnessPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.sid.fitness_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessPlannerApplication.class, args);
+	}
+
+}
